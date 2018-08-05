068a56812843c9b8a8f136be7878acb42d9db8c1
frederic-f <=>
2018-08-05 17:56:15 +0200

    GUI epuree

--- clusterGUI.py
+++ clusterGUI.py
@@ -1,34 +1,83 @@
 # https://www.python-course.eu/python_tkinter.php
 
+import os
 import wol
 
 from tkinter import *
+
 fields = 'Last Name', 'First Name', 'Job', 'Country'
 
+hosts = 'horus', 'lenovo'
+
 def fetch(entries):
-   for entry in entries:
-      field = entry[0]
-      text  = entry[1].get()
-      print('%s: "%s"' % (field, text))
+
+    for entry in entries:
+
+        field = entry[0]
+
+        text  = entry[1].get()
+
+        print('%s: "%s"' % (field, text))
+
 
 def makeform(root, fields):
-   entries = []
-   for field in fields:
-      row = Frame(root)
-      lab = Label(row, width=15, text=field, anchor='w')
-      ent = Entry(row)
-      row.pack(side=TOP, fill=X, padx=5, pady=5)
-      lab.pack(side=LEFT)
-      ent.pack(side=RIGHT, expand=YES, fill=X)
-      entries.append((field, ent))
-   return entries
 
+    entries = []
+
+    for field in fields:
+
+        row = Frame(root)
+
+        lab = Label(row, width=15, text=field, anchor='w')
+
+        ent = Entry(row)
+
+        row.pack(side=TOP, fill=X, padx=5, pady=5)
+
+        lab.pack(side=LEFT)
+
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+
+        entries.append((field, ent))
+
+    return entries
+
+
+def listHosts(root, hosts):
+
+    entries = []
+
+    for host in hosts:
+
+        row = Frame(root)
+
+        lab1 = Label(row, width=15, text=host, anchor='w')
+
+        # checkk if host is up
+        HOST_UP = "Up" if os.system("ping -c 1 " + host) is 0 else "Down"
+
+        lab2 = Label(row, width=15, text=HOST_UP, anchor='w')
+
+        #ent = Entry(row)
+
+        row.pack(side=TOP, fill=X, padx=5, pady=5)
+
+        lab1.pack(side=LEFT)
+
+        lab2.pack(side=RIGHT)
+
+        #ent.pack(side=RIGHT, expand=YES, fill=X)
+
+        #entries.append((host, ent))
+
+    return True
 
 def startNode(node):
 
     print("starting %s" % node)
 
     wol.wake_on_lan(node)
+
 
 def startCluster():
 
@@ -44,23 +93,33 @@ def startCluster():
 
 if __name__ == '__main__':
 
-   root = Tk()
+    # init Tk
+    root = Tk()
 
-   ents = makeform(root, fields)
+    # set window title
+    root.wm_title("Cluster Manager")
 
-   root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+    # Enter key
+    root.bind('<Return>', (lambda event: startCluster()))
 
-   b1 = Button(root, text='Show', command=(lambda e=ents: fetch(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
+    # Esc key
+    root.bind('<Escape>', (lambda event: root.quit()))
 
-   b2 = Button(root, text='Quit', command=root.quit)
-   b2.pack(side=LEFT, padx=5, pady=5)
+    ent = listHosts(root, hosts)
 
-   b3 = Button(root, text='Start lenovo', command=(lambda: startNode("lenovo")))
-   b3.pack(side=LEFT, padx=5, pady=5)
+    #b1 = Button(root, text='Show', command=(lambda e=ents: fetch(e)))
+    #b1.pack(side=LEFT, padx=5, pady=5)
 
-   b4 = Button(root, text='Start cluster', command=(lambda: startCluster()))
-   b4.pack(side=LEFT, padx=5, pady=5)
+    #b2 = Button(root, text='Quit', command=root.quit)
+    #b2.pack(side=LEFT, padx=5, pady=5)
+
+    row = Frame(root)
+
+    b3 = Button(root, text='Start lenovo', command=(lambda: startNode("lenovo")))
+    b3.pack(side=LEFT, padx=5, pady=5)
+
+    b4 = Button(root, text='Start cluster', command=(lambda: startCluster()))
+    b4.pack(side=LEFT, padx=5, pady=5)
 
 
-   root.mainloop()
+    root.mainloop()
